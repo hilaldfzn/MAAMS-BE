f3e70ddc540c7269d40f7d3d5e8ad8b0ca1ed347
rania <raniamn03@gmail.com>
2024-02-23 18:51:31 +0700

    added skeleton function for question API

---
+++ validator/views/question.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+
+class Question(APIView):
+    def get():
+        return ""
+    
+    def put():
+        return ""
+    
+    def delete():
+        return ""
