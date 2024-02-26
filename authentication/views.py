ab60fed87f10be61649afdc54f19c5061330f594
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2024-02-26 10:06:08 +0700

    feat(login): routing, create base login function for tdd

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+def login(request):
+    """
+    Process login credential requests.
+    """
+    if request.method == 'POST':
+        
+        return Response(
+            data={ 
+                "access_token": "<access_token>",
+                "refresh_token": "<access_token>",
+                "data": "<user_data>",
+                "detail": "Successfully logged in."
+            }, 
+            status=status.HTTP_200_OK
+        )
