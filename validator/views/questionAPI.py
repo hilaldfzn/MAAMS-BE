f6cd7c7e7efb5ab4968f8d5ceb49eb98e5eab220
rania <raniamn03@gmail.com>
2024-02-27 00:08:39 +0700

    chore(validator): remove unused imports

--- validator/views/questionAPI.py
+++ validator/views/questionAPI.py
@@ -1,7 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from validator.serializers import QuestionRequest, QuestionResponse, BaseQuestion
-from rest_framework import status
 
 class QuestionAPI(APIView):
     def post(self, request):
