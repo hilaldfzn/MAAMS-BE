ac5a54b4ad2aa9040e49fa279a6f66cbd8fb13c9
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2024-02-26 10:32:55 +0700

    fix(login): routing, set login function as api view

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+@api_view(['POST'])
 def login(request):
     """
     Process login credential requests.
