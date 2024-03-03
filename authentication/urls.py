885f61c8f177316823e391b121d4c046ceaea4d1
adlyrndi <adlyyrr12@gmail.com>
2024-03-03 16:55:07 +0700

    feat(logout): Implement logout backend logic and fixing routing logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView
+)
 
 from .views import (
     login, 
@@ -9,5 +12,6 @@ app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),    
-    path('register/', register, name='register')   
+    path('register/', register, name='register'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout')      
 ]
