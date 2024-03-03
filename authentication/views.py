8c0eb13535c48ffe6118806f7e7178a887574a2b
adlyrndi <adlyyrr12@gmail.com>
2024-03-03 15:59:05 +0700

    style(register): remove unsued import and removing unused comment

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,15 @@
-from django.contrib.auth.hashers import check_password, make_password
+from django.contrib.auth.hashers import check_password
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.exceptions import (
    AuthenticationFailed, ParseError
 )
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from drf_spectacular.utils import extend_schema
 
@@ -74,7 +73,6 @@ def generate_tokens(user: CustomUser) -> dict:
     }
 
 @extend_schema(
-    # description='Fill with valid username and password for an existing user.',
     request=RegisterSerializer,
 )
 @api_view(['POST'])
@@ -82,10 +80,10 @@ def register(request):
     """
     Register a new user.
     """
-    if request.method == 'POST':  # Checking the request method
+    if request.method == 'POST':  
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             return Response({"detail": "User registered successfully."}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Handling response when serializer is not valid
-    return Response({"detail": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)  # Handling response when method is not allowed
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+    return Response({"detail": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)  
