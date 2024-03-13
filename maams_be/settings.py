939572674f25669548c108ba30c003c362e6dc08
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2024-03-13 10:16:28 +0700

    chore(logs): configure logging from console

--- maams_be/settings.py
+++ maams_be/settings.py
@@ -155,6 +155,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'maams_be.wsgi.application'
 
+# Logging
+# https://stackoverflow.com/a/21993077/16568197
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
