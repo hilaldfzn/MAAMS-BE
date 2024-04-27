b2da7418f337166d6b1e5f07be429e3dadc6ec78
rania <raniamn03@gmail.com>
2024-04-27 23:55:27 +0700

    [RED] feat(validator): create test case for updating question's attribute with same value

--- validator/exceptions.py
+++ validator/exceptions.py
@@ -12,3 +12,6 @@ class InvalidTimeRangeRequestException(APIException):
 
 class EmptyTagException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
+    
+class ValueNotUpdatedException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
