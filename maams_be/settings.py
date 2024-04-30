3c63b2f627776dde5d140ded2fcab36123ce8d81
bagas-shalahuddin <bagas.shalahuddin@ui.ac.id>
2024-04-30 23:01:59 +0700

    feat(validator): [green] implement new api call using claude

--- maams_be/settings.py
+++ maams_be/settings.py
@@ -41,17 +41,21 @@ def get_env_value(env_variable: str) -> str | int | bool | None:
     Gets environment variables depending on active environment.
     """
     try:
-        value = parse_env_value(os.environ[env_variable])
+        value = parse_env_value(env_variable, os.environ[env_variable])
         return value
     except KeyError:
         error_msg = f'{env_variable} environment variable not set.'
         raise ImproperlyConfigured(error_msg)
 
-def parse_env_value(value: str) -> str | bool | int | None:
+def parse_env_value(key: str, value: str) -> str | bool | int | None:
     """
     Parses environment variable into either bool, strings, ints, or None type.
     """
-    value = value.lower()
+    case_sensitive_keys = ["CLAUDE_API_KEY"]
+    
+    if key not in case_sensitive_keys:
+        value = value.lower()
+        
     if value == "none": return None             # Checks for None type
     if value in ["0", "false"]: return False    # Checks for bool types
     if value in ["1", "true"]: return True
@@ -238,7 +242,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-OPENAI_API_KEY = get_env_value("OPENAI_API_KEY")
+CLAUDE_API_KEY = get_env_value("CLAUDE_API_KEY")
 
 # Sentry
 if active_env == "DEVELOPMENT":
--- validator/services/causes.py
+++ validator/services/causes.py
@@ -1,4 +1,4 @@
-import openai
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from authentication.models import CustomUser
@@ -6,25 +6,33 @@ from validator.dataclasses.create_cause import CreateCauseDataClass
 from validator.models import question
 from validator.models.causes import Causes
 from validator.services import question
-from validator.exceptions import NotFoundRequestException, ForbiddenRequestException
+from validator.exceptions import NotFoundRequestException, ForbiddenRequestException, AIServiceErrorException
 from validator.constants import ErrorMsg
 import uuid
-
+import requests
 
 class CausesService:
     def api_call(self, prompt: str):
-        openai.api_key = settings.OPENAI_API_KEY
-        response = openai.Completion.create(
-                model="gpt-3.5-turbo",
-                prompt=prompt,
-                max_tokens=1,  
-                n=3,           
-                temperature=0  
+        client = Anthropic(api_key=settings.CLAUDE_API_KEY)
+        full_prompt = f"{HUMAN_PROMPT} {prompt} {AI_PROMPT}"
+
+        try:
+            response = client.completions.create(
+                model="claude-2.1",
+                max_tokens_to_sample=300,
+                prompt=full_prompt,
             )
-        answer = response.choices[0].text.strip()
-
-        return answer
-
+            
+            answer = response.completion.strip()
+            
+        except requests.exceptions.RequestException:
+            raise AIServiceErrorException(ErrorMsg.AI_SERVICE_ERROR)
+            
+        if answer.startswith('True'):
+            return True
+        elif answer.startswith('False'):
+            return False
+    
     def validate(self, question_id: uuid):
         max_row = Causes.objects.filter(problem_id=question_id).order_by('-row').values_list('row', flat=True).first()
         causes = Causes.objects.filter(problem_id=question_id, row=max_row)
@@ -32,15 +40,15 @@ class CausesService:
 
         for cause in causes:
             if max_row == 1:
-                prompt = f"Is '{cause.cause}' the cause of '{problem.question}'? Answer using True/False"
-                if self.api_call(prompt):
+                prompt = f"Is '{cause.cause}' the cause of '{problem.question}'? Answer using only True/False"
+                if self.api_call(self=self, prompt=prompt):
                     cause.status = True
                     cause.save()
             else:
                 prev_cause = Causes.objects.filter(problem_id=question_id, row=max_row-1, column=cause.column).first()
                 if prev_cause and prev_cause.cause == cause.cause:
-                    prompt = f"Is '{cause.cause}' the cause of '{prev_cause.cause}'? Answer using True/False"
-                    if self.api_call(prompt):
+                    prompt = f"Is '{cause.cause}' the cause of '{prev_cause.cause}'? Answer using only True/False"
+                    if self.api_call(self=self, prompt=prompt):
                         cause.status = True
                         cause.save()
 
